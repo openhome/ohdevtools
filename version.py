0acf2db5526d399f4845496e46f95aa9b56b2222
grahamdo <graham.douglas@linn.co.uk>
2015-10-23 10:06:37 +0100

    Reword bad ohdevtools fail message to cause integration tests to detect and log failure if this occurs

--- version.py
+++ version.py
@@ -4,7 +4,7 @@ import sys
 
 # The version number of the API. Incremented whenever there
 # are new features or bug fixes.
-VERSION = 53
+VERSION = 54
 
 # The earliest API version that we're still compatible with.
 # Changed only when a change breaks an existing API.
@@ -18,9 +18,9 @@ class BadVersionException(Exception):
 def require_version(required_version):
     '''Fail if the version of ohDevTools is too old.'''
     if VERSION<required_version:
-        raise BadVersionException("This project requires a newer version of ohDevTools. You have version {0}, but need version {1}.".format(VERSION, required_version))
+        raise BadVersionException("FAIL: This project requires a newer version of ohDevTools. You have version {0}, but need version {1}.".format(VERSION, required_version))
     if required_version<BACKWARD_VERSION:
-        raise BadVersionException("This project requires an older version of ohDevTools. You have version {0}, but need version {1}.".format(VERSION, required_version))
+        raise BadVersionException("FAIL: This project requires an older version of ohDevTools. You have version {0}, but need version {1}.".format(VERSION, required_version))
 
 def check_version():
     if os.path.isfile('projectdata/ohdevtools.json'):
