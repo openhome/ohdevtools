b3bb0e9fe10cc3319c907530c5afcae8ce55202d
grahamdo <graham.douglas@linn.co.uk>
2016-01-13 10:00:06 +0000

    Add dependency version cross-check

--- dependencies.py
+++ dependencies.py
@@ -13,6 +13,7 @@ import stat
 import tempfile
 from glob import glob
 from default_platform import default_platform
+import deps_cross_checker
 
 # Master table of dependency types.
 
@@ -921,5 +922,13 @@ def fetch_dependencies(dependency_names=None, platform=None, env=None, fetch=Tru
                 if nuget_config is not None and os.path.isfile(nuget_config):
                     args += ['-ConfigFile', nuget_config]
                 cli(args)
-                
+
+    # Finally perform cross-check of (major.minor) dependency versions to ensure that these are in sync
+    # across this (current) repo and all its pulled-in dependencies. Done as totally seperate operation
+    # to isolate from the main fetcher code to assist with any future maintenance
+    xcheck = deps_cross_checker.DepsCrossChecker()
+    result = xcheck.execute()
+    if result != 0:
+        raise Exception( 'Failed: dependency cross-checker detected problem(s)' )
+
     return dependencies
---
+++ deps_cross_checker.py
@@ -0,0 +1,55 @@
+"""Class to perform cross check of dependency versions"""
+import json
+import os
+
+kDepsFilename = 'dependencies.json'
+
+class DepsCrossChecker:
+    """Ensure version consistency (at major.minor level) across all dependencies"""
+
+    def __init__( self ):
+        """Initialise class data"""
+        self.failures  = 0
+        self.artifacts = {}
+
+    def execute( self ):
+        """Perform the check - return zero on success, number of mismatches on failure"""
+        print 'Cross-checking depency versions'
+        print '  Finding %s dependency definition files...' % kDepsFilename
+        for root, dirs, files in os.walk( os.getcwd() ):
+            for name in files:
+                if name == kDepsFilename:
+                    self.artifacts[os.path.basename( root )] = self.parse_json( os.path.join( root, kDepsFilename ))
+
+        projects = self.artifacts.keys()
+        for project1 in projects:
+            for project2 in projects:
+                if project1 != project2:
+                    self.check_versions( project1, project2 )
+            projects.remove( project1 )
+        return self.failures
+
+    def check_versions( self, aProject1, aProject2 ):
+        """Perform comparison between 2 specified projects"""
+        print '    Checking %s against %s' % (aProject1, aProject2)
+        for dependency in self.artifacts[aProject1]:
+            if dependency in self.artifacts[aProject2]:
+                version1 = self.artifacts[aProject1][dependency]
+                version2 = self.artifacts[aProject2][dependency]
+                if version1==version2:
+                    print '      %-16s %6s        --> OK' % (dependency, version1)
+                else:
+                    self.failures += 1
+                    print '      %-16s %6s/%-6s --> FAILED' % (dependency, version1, version2)
+
+    @staticmethod
+    def parse_json( aPath ):
+        """Read and parse the JSON dependencies file"""
+        deps = {}
+        if os.path.exists( aPath ):
+            f = open( aPath, 'rt' )
+            items = json.load( f )
+            f.close()
+            for item in items:
+                deps[item['name'].encode( 'ascii' )] =  '.' . join( item['version'].split( '.' )[:-1] ).encode( 'ascii' )
+        return deps
