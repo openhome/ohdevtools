b286669c81e3f4742b78854916173eed39a216b9
grahamdo <graham.douglas@linn.co.uk>
2017-07-31 13:51:48 +0100

    Fix typos

--- ci_build.py
+++ ci_build.py
@@ -34,9 +34,9 @@ def get_vsvars_environment(architecture="x86"):
 
     for vcvars32 in [
         'C:\\Program Files (x86)\\Microsoft Visual Studio 12.0\\Common7\\Tools\\vsvars32.bat',                      # VS12 Express
-        'C:\\Program Files\Microsoft Visual Studio\\2017\\Professional\\Common7\\Tools\\vcvars32.bat'               # VS2017 Pro
-        'C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\BuildTools\\VC\\Auxilliary\\Build\\vcvars32.bat']: # Build Tools for VS2017
-        if os.path.isfile( vcvars32 ):
+        'C:\\Program Files\\Microsoft Visual Studio\\2017\\Professional\\Common7\\Tools\\vcvars32.bat',               # VS2017 Pro
+        'C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\BuildTools\\VC\\Auxiliary\\Build\\vcvars32.bat']: # Build Tools for VS2017
+        if os.path.isfile(vcvars32):
             process = subprocess.Popen('("%s" %s>nul)&&"%s" -c "import os; print repr(os.environ)"' % (vcvars32, architecture, python), stdout=subprocess.PIPE, shell=True)
             stdout, _ = process.communicate()
             exitcode = process.wait()
