32a3371f38626262e9dfc57ee3beee20f7ad6343
grahamdo <graham.douglas@linn.co.uk>
2018-01-12 16:09:50 +0000

    Add Raspberry Pi support to dependency fetcher, including host-detection of Pi

--- default_platform.py
+++ default_platform.py
@@ -1,15 +1,23 @@
+import os
 import platform
+
 
 def default_platform():
     if platform.system() == 'Windows':      # Note ALL windows environment treated as 32-bit
         return 'Windows-x86'
-    if platform.system() == 'Linux' and platform.architecture()[0] == '32bit' and platform.machine()[0:3] == 'ppc':
-        return 'Linux-ppc32'
-    if platform.system() == 'Linux' and platform.architecture()[0] == '32bit':
-        if platform.machine() == 'armv7l':
-            return 'Linux-armhf'
-        else:
-            return 'Linux-x86'
+    if platform.system() == 'Linux':
+        if platform.architecture()[0] == '32bit':
+            if platform.machine()[0:3] == 'ppc':
+                return 'Linux-ppc32'
+            elif platform.machine() == 'armv7l':
+                if os.path.exists('/sys/firmware/devicetree/base/model'):
+                    with open('/sys/firmware/devicetree/base/model', 'r') as f:
+                        info = f.read()
+                    if 'Raspberry Pi' in info:
+                        return 'Linux-rpi'
+                return 'Linux-armhf'
+            else:
+                return 'Linux-x86'
     if platform.system() == 'Linux' and platform.architecture()[0] == '64bit':
         return 'Linux-x64'
     if platform.system() == 'Darwin':
