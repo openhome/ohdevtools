5259272423b8566f898acebacf802f231a3ff5f4
joshh <josh.hahn@linn.co.uk>
2018-10-03 16:55:31 +0100

    added sort option to aws ls

--- aws.py
+++ aws.py
@@ -51,8 +51,46 @@ def copy(aSrc, aDst):
             else:
                 bucket.upload_fileobj(data, '/'.join(aDst.split('/')[3:]))
 
+def delete(aBucket, aKey):
+    if aKey != None and len(aKey) > 0:
+        s3 = boto3.resource('s3')
+        bucket = s3.Bucket(aBucket)
+        # this allows a single file to be deleted or an entire directory, so be careful!
+        bucket.objects.filter(Prefix=aKey).delete()
 
-def ls(aUri):
+def CompareVersions(aVersion1, aVersion2):
+    # if aVersion1 > aVersion2 return true, otherwise return false
+    aVersion1 = aVersion1.strip("/").split("/")[-1]
+    aVersion2 = aVersion2.strip("/").split("/")[-1]
+
+    try:
+        v1valid = len(aVersion1.split('.')) == 3 and aVersion1.split('.')[0] != "0"
+    except:
+        v1valid = False
+    try:
+        v2valid = len(aVersion2.split('.')) == 3 and aVersion2.split('.')[0] != "0"
+    except:
+        v2valid = False
+
+    if v1valid and not v2valid:
+        return 1
+    elif v2valid and not v1valid:
+        return -1
+    elif not v1valid and not v2valid:
+        return 0
+
+    new = aVersion1.split('_')[0].split('.')
+    old = aVersion2.split('_')[0].split('.')
+
+    if int(new[0]) > int(old[0]):
+        return 1
+    elif int(new[0]) == int(old[0]) and int(new[1]) > int(old[1]):
+        return 1
+    elif int(new[0]) == int(old[0]) and int(new[1]) == int(old[1]) and int(new[2]) > int(old[2]):
+        return 1
+    return -1
+
+def ls(aUri, aSort=None):
     """Return directory listing of contents of specified URI"""
     entries = []
     fields = aUri.split('/')
@@ -68,11 +106,8 @@ def ls(aUri):
     if 'Contents' in objects:
         for item in objects['Contents']:
             entries.append(item['Key'])
+    if 'asc' in aSort.lower():
+        entries = sorted(entries, cmp=CompareVersions)
+    elif 'desc' in aSort.lower():
+        entries = sorted(entries, cmp=CompareVersions, reverse=True)
     return entries
-
-def delete(aBucket, aKey):
-    if aKey != None and len(aKey) > 0:
-        s3 = boto3.resource('s3')
-        bucket = s3.Bucket(aBucket)
-        # this allows a single file to be deleted or an entire directory, so be careful!
-        bucket.objects.filter(Prefix=aKey).delete()
