9f602d2f48533df7212b6e2dcfb45f5f94516396
Graham Douglas <GrahamDo@linn.co.uk>
2019-01-08 14:33:45 +0000

    Detect if running on AWS slave - only attempt to generate AWS credentials of NOT running on AWS slave

--- aws.py
+++ aws.py
@@ -1,6 +1,11 @@
 """ Interface to AWS S3 storage"""
+import json
 import os
+import requests
 import sys
+
+kAwsBucketPrivate   = 'linn-artifacts-private'
+kAwsMetadataService = 'http://169.254.169.254/latest/meta-data/iam/info'
 
 try:
     import boto3
@@ -8,30 +13,41 @@ except:
     print('\nAWS fetch requires boto3 module')
     print("Please install this using 'pip install boto3'\n")
 else:
-    # create AWS credentials file (if not already present)
-    home = None
-    if 'HOMEPATH' in os.environ and 'HOMEDRIVE' in os.environ:
-        home = os.path.join(os.environ['HOMEDRIVE'], os.environ['HOMEPATH'])
-    elif 'HOME' in os.environ:
-        home = os.environ['HOME']
-    if home:
-        awsCreds = os.path.join(home, '.aws', 'credentials')
-        if not os.path.exists(awsCreds):
-            if sys.version_info[0] == 2:
-                from urllib2 import urlopen
-            else:
-                from urllib.request import urlopen
-            try:
-                os.mkdir(os.path.join(home, '.aws'))
-            except:
-                pass
-            try:
-                credsFile = urlopen('http://core.linn.co.uk/aws-credentials' )
-                creds = credsFile.read()
-                with open(awsCreds, 'wt') as f:
-                    f.write(creds)
-            except:
-                pass
+    awsSlave = False
+    try:
+        resp = requests.get( kAwsMetadataService )
+        meta = json.loads( resp.text )
+        if 'InstanceProfileArn' in meta:
+            if 'dev-tools-EC2SlaveInstanceProfile' in meta['InstanceProfileArn']:
+                awsSlave = True
+    except:
+        pass
+
+    if not awsSlave:
+        # create AWS credentials file (if not already present)
+        home = None
+        if 'HOMEPATH' in os.environ and 'HOMEDRIVE' in os.environ:
+            home = os.path.join(os.environ['HOMEDRIVE'], os.environ['HOMEPATH'])
+        elif 'HOME' in os.environ:
+            home = os.environ['HOME']
+        if home:
+            awsCreds = os.path.join(home, '.aws', 'credentials')
+            if not os.path.exists(awsCreds):
+                if sys.version_info[0] == 2:
+                    from urllib2 import urlopen
+                else:
+                    from urllib.request import urlopen
+                try:
+                    os.mkdir(os.path.join(home, '.aws'))
+                except:
+                    pass
+                try:
+                    credsFile = urlopen('http://core.linn.co.uk/aws-credentials' )
+                    creds = credsFile.read()
+                    with open(awsCreds, 'wt') as f:
+                        f.write(creds)
+                except:
+                    pass
 
 def copy(aSrc, aDst):
     """Copy objects to/from AWS. AWS uri in form s3://<bucket>/<key>"""
@@ -45,17 +61,19 @@ def copy(aSrc, aDst):
         bucket = resource.Bucket(aDst.split('/')[2])
         with open( aSrc, 'rb' ) as data:
             ext = aSrc.split(".")[-1]
-            if ext in  ["txt", "json", "xml"]:
+            if ext in ["txt", "json", "xml"]:
                 bucket.upload_fileobj(data, '/'.join(aDst.split('/')[3:]), ExtraArgs={'ContentType': 'text/plain'})
             else:
                 bucket.upload_fileobj(data, '/'.join(aDst.split('/')[3:]))
 
+
 def delete(aBucket, aKey):
-    if aKey != None and len(aKey) > 0:
+    if aKey is not None and len(aKey) > 0:
         s3 = boto3.resource('s3')
         bucket = s3.Bucket(aBucket)
         # this allows a single file to be deleted or an entire directory, so be careful!
         bucket.objects.filter(Prefix=aKey).delete()
+
 
 def CompareVersions(aVersion1, aVersion2):
     # if aVersion1 > aVersion2 return true, otherwise return false
@@ -89,6 +107,7 @@ def CompareVersions(aVersion1, aVersion2):
         return 1
     return -1
 
+
 def ls(aUri, aSort=None):
     """Return directory listing of contents of specified URI"""
     entries = []
@@ -105,12 +124,13 @@ def ls(aUri, aSort=None):
     if 'Contents' in objects:
         for item in objects['Contents']:
             entries.append(item['Key'])
-    if aSort != None:
+    if aSort is not None:
         if 'asc' in aSort.lower():
             entries = sorted(entries, cmp=CompareVersions)
         elif 'desc' in aSort.lower():
             entries = sorted(entries, cmp=CompareVersions, reverse=True)
     return entries
+
 
 def exists(aUri):
     bucket = aUri.split('/')[2]
@@ -122,7 +142,7 @@ def exists(aUri):
     except:
         return False
 
-kAwsBucketPrivate = 'linn-artifacts-private'
+
 def download( aKey, aDestinationFile, aBucket=kAwsBucketPrivate ):
     print( 'Download from AWS s3://%s/%s to %s' % ( aBucket, aKey.strip("/"), os.path.abspath( aDestinationFile ) ) )
     s3 = boto3.resource( 's3' )
